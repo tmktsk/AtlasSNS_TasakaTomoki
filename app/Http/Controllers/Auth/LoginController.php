e2cffd55216ad4e25682e86cf9371549a0994d9c
tmktsk <137078939+tmktsk@users.noreply.github.com>
2023-08-20 19:02:30 +0900

    スクール内修正

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
-use Auth;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,18 +4,19 @@
 <div class="container">
     {!! Form::open(['url' => '/create', 'method' => 'POST']) !!}
     <!-- <div class="form-group"> -->
-        {{ Form::input('text', 'post', null, ['required', 'class' => 'post-form', 'placeholder' => '投稿内容を入力してください。']) }}
+    {{ Form::input('text', 'post', null, ['required', 'class' => 'post-form', 'placeholder' => '投稿内容を入力してください。']) }}
     <!-- </div> -->
     <input type="image" src="images/post.png" class="post-img">
-{!! Form::close() !!} 
-<img src="{{ asset('images/' . Auth::user()->images) }}" alt="User Icon" class="postIcon">
+    {!! Form::close() !!}
+    <img src="{{ asset('images/' . Auth::user()->images) }}" alt="User Icon" class="postIcon">
 
-<div class="post-content">
-    @if(session('postContent'))
+    <div class="post-content">
+        @if(session('postContent'))
         <div class="posted-content">
             {{ session('postContent') }}
         </div>
-    @endif
+        @endif
+    </div>
 </div>
 
 @endsection
