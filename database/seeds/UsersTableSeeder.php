6fa0bf6ce8cfdac3940c11751a7bf00d655e8725
tsktmk <tmktsk@users.noreply.github.com>
2023-12-09 21:43:48 +0900

    seederファイル編集済み

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,13 +13,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create();
+
         DB::table('users')->insert([
             [
                 'username' => 'TasakaTomoki',
                 'mail' => 'tigertomo228@ab.auone-net.jp',
                 'password' => Hash::make('TomoAtlas228'),
+                'images' => 'images/icon' . $faker->numberBetween(1, 7) . '.png',
             ]
-            
+
         ]);
     }
 }
--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -18,14 +18,16 @@
   @if($posts->count() > 0)
     @foreach ($posts as $post)
       <div class="fw-box">
-        @if(optional($post->user)->check)
-          <img src="{{ asset('storage/'. $post->user->images) }}" alt="User Icon" class="fw-userIcon">
-        @else
-          <img src="{{ asset('images/'. $post->user->images) }}" alt="User Icon" class="fw-userIcon">
-        @endif
+          @if(optional($post->user)->check)
+            <img src="{{ asset('storage/'. $post->user->images) }}" alt="User Icon" class="fw-userIcon">
+          @else
+            <img src="{{ asset('images/'. $post->user->images) }}" alt="User Icon" class="fw-userIcon">
+          @endif
+        <div class="fw-block">
           <p class="fw-username">{{ $post->user->username }}</p>
-          <p class="fw-timestamp">{{ $post->created_at->format('Y-m-d H:i') }}</p>
           <p class="fw-post">{{ $post->post }}</p>
+        </div>
+          <p class="fw-timestamp">{{ $post->created_at->format('Y-m-d H:i') }}</p>
       </div>
     @endforeach
   @endif
